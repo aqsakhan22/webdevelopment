ab401971544462069be33f64f8dbb316aa8fc483
aqsakhan22 <toheed.wajid@bmacapital.com>
2023-11-15 11:57:03 +0500

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  console.log('Getting file');
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+     io.emit('chat message', msg);
+  });
+  // socket.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
+});
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
